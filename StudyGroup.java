7703cad7c19ac20e20aeb099aef7dc29cb9eb9ce
ZakiaDimuk <dimukasheva97@mail.ru>
2023-08-05 02:17:44 +0500

    Seminar

---
+++ StudyGroup.java
@@ -0,0 +1,13 @@
+package OOP_Sem5;
+
+import java.util.List;
+
+public class StudyGroup {
+    private Teacher teacher;
+    private List<Student> students;
+
+    public StudyGroup(Teacher teacher, List<Student> students){
+        this.teacher = teacher;
+        this.students = students;
+    }
+}
